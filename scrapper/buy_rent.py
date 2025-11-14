80a6d78b58f9343b47b4cb1d7c871a2a7da17e92
AlphaTheDog <alphathedogist@gmail.com>
2025-11-14 15:48:48 +0300

    added pagination both to notebook and scrapper

--- scrapper/buy_rent.py
+++ scrapper/buy_rent.py
@@ -22,13 +22,8 @@ time.sleep(10)  # Wait for the page to load completely
 cookies=driver.find_element(By.ID, "onetrust-accept-btn-handler")
 driver.execute_script("arguments[0].scrollIntoView()",cookies)
 cookies.click()
-wait=WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, "listing-card")))
 
-page_soup=BeautifulSoup(driver.page_source, "html.parser")
-
-def house_details(soup):
-    data=[]
+def houses(soup):
     listings=soup.find_all('div','listing-card')
     for listing in listings:
         container=listing.find('div','md:w-3/5 relative flex flex-col justify-between px-3 py-4 md:px-5')
@@ -40,8 +35,15 @@ def house_details(soup):
         desc=house_details.find('h3','block flex-1 text-sm font-medium leading-5 text-black text-grey-850 md:hidden').text.strip()
         location=house_details.find('div','flex max-w-full items-center').p.text.strip()
         house_details.find('h3','block flex-1 text-sm font-medium leading-5 text-black text-grey-850 md:hidden').text.strip()
-        price_details=house_details.find('h3','capitalize flex').find('div','flex items-center justify-center text-xl font-bold leading-7 text-grey-900')
-        price=price_details.find('a').get_text(strip=True)
+        price_div=house_details.find('h3','capitalize flex')
+        classes=[
+            'flex items-center justify-center text-xl font-bold leading-7 text-grey-900',
+            'inline-flex justify-between text-xl font-bold leading-7 text-grey-850 md:hidden']
+        for cls in classes:
+            price_details=price_div.find('div',cls)
+            if price_details:
+                price=price_details.find('a').get_text(strip=True)
+                break
         washrooms=house_details.find('div','swiper-wrapper space-x-2')
         try:
             beds=washrooms.find('div','swiper-slide flex h-6 !w-auto items-center rounded-full bg-highlight px-2 py-1 text-sm font-normal leading-4 text-grey-550 swiper-slide-active')
@@ -70,12 +72,34 @@ def house_details(soup):
             'house_link':house_link
             }
             )
-    return data
+
+
+def click_next_page():
+    next_cont=driver.find_element(By.XPATH, "//*[contains(@class, 'mt-4') and contains(@class, 'flex') and contains(@class, 'w-full') and contains(@class, 'flex-row') and contains(@class, 'items-center') and contains(@class, 'justify-center') and contains(@class, 'space-x-1') and contains(@class, 'md:space-x-3')]")
+    next_link=next_cont.find_elements(By.TAG_NAME,'a')
+    driver.execute_script("arguments[0].click()",next_link[-1])
+    
+
+
+paginater=driver.find_element(By.CLASS_NAME,'mb-5').find_element(By.XPATH,"//ul[contains(@class, 'list-reset') and contains(@class, 'pagination-page-nav') and contains(@class, 'flex') and contains(@class, 'w-auto') and contains(@class, 'justify-center') and contains(@class, 'space-x-1') and contains(@class, 'p-0') and contains(@class, 'md:space-x-3')]")
+all_pages=paginater.find_elements(By.TAG_NAME,"li")
+pages=int(all_pages[-1].find_element(By.TAG_NAME,'a').text)
+
+data=[]
+for page in range(3):
+    wait=WebDriverWait(driver, 20)
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "listing-card")))
+    page_soup=BeautifulSoup(driver.page_source, "html.parser")
+    houses(page_soup)
+    click_next_page()
+
+
 fields=['title', 'desc', 'location', 'area', 'price', 'bedrooms', 'bathrooms','house_link']
-data = house_details(page_soup)
 with open('data/houses.csv','w',newline='',encoding='utf-8') as obj:
     writer=csv.DictWriter(obj,fieldnames=fields)
     writer.writeheader()
     writer.writerows(data)
-    
+
+
 driver.quit()
+
